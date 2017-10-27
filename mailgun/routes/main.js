10e6e0d46fea26c89387bb38d4011d485eec7b27
Rodolfo Martinez <rudy.7.mtz@gmail.com>
2017-10-27 13:13:19 -0500

    Initialize mailgun

--- mailgun/routes/main.js
+++ mailgun/routes/main.js
@@ -11,10 +11,31 @@ var controllers = {
     res.render("index", context);
   },
 
+	sendMail: function(req, res, next){
+
+		var correo = req.body.correo;
+		console.log(correo);
+
+    var mail = new Mailgunny({
+      domain: 'brounieapps.com',
+      key: 'key-21c7e8594275cf82882064cebb71aa52'
+    });
+
+    mail.send({
+      from: 'robot@brounie.com',
+      to: ['rudy@brounie.com'],
+      subject: 'Prueba',
+      html: "<p>prueba</p>" +correo 
+    },function(error, body){
+        console.log(error);
+        res.redirect("/");
+    });
+		res.redirect("/");
+  }
 
 }
 
 router.get('/', controllers.index);
-
+router.post('/sendMail', controllers.sendMail);
 
 module.exports = router;
